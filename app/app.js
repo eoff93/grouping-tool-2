d741612845a0a323b29915b5cf0c61ffe44e2682
eoff93 <bane.dev101@gmail.com>
2016-07-16 11:45:22 +0200

    Add glyphicons for sorting

--- app/app.js
+++ app/app.js
@@ -2,27 +2,27 @@ angular.module('groupApp', ['checklist-model'])
 .factory('getData', function() {
   return {
     groups: [
-      {"id": 1,"name": "Social sites", "color": "Red", "sites": ["shop-pro.jp", "scribd.com"] },
+      {"id": 1,"name": "Social sites", "color": "red", "sites": ["shop-pro.jp", "scribd.com"] },
       {"id": 2,"name": "Search engines", "color": "", "sites": ["google.com", "linkedin.com"] },
-      {"id": 3,"name": "Blogs", "color": "Green", "sites": ["live.com", "berkeley.edu", "hibu.com"] },
-      {"id": 4,"name": "Learning sites", "color": "Orange", "sites": ["shop-pro.jp"] },
-      {"id": 5,"name": "Entertainment", "color": "Red", "sites": null }
+      {"id": 3,"name": "Blogs", "color": "green", "sites": ["live.com", "berkeley.edu", "hibu.com"] },
+      {"id": 4,"name": "Learning sites", "color": "orange", "sites": ["shop-pro.jp"] },
+      {"id": 5,"name": "Entertainment", "color": "red", "sites": null }
     ],
 
     sites: [
-      {"id": 1, "url": "shop-pro.jp", "color": "Red","groups": ["Social sites", "Learning sites"]},
-      {"id": 2, "url": "desdev.cn", "color": "Blue", "groups": null},
-      {"id": 3, "url": "live.com", "color": "Orange", "groups": ["Blogs"]},
-      {"id": 4, "url": "microsoft.com", "color": "Red", "groups": null},
-      {"id": 5, "url": "scribd.com", "color": "Yellow", "groups": ["Social sites"]},
+      {"id": 1, "url": "shop-pro.jp", "color": "red","groups": ["Social sites", "Learning sites"]},
+      {"id": 2, "url": "desdev.cn", "color": "blue", "groups": null},
+      {"id": 3, "url": "live.com", "color": "orange", "groups": ["Blogs"]},
+      {"id": 4, "url": "microsoft.com", "color": "red", "groups": null},
+      {"id": 5, "url": "scribd.com", "color": "yellow", "groups": ["Social sites"]},
       {"id": 6, "url": "linkedin.com", "color": "", "groups": ["Search engines"]},
       {"id": 7, "url": "berkeley.edu", "color": "Goldenrod", "groups": ["Blogs"]},
-      {"id": 8, "url": "google.com", "color": "Blue", "groups": ["Search engines"]},
+      {"id": 8, "url": "google.com", "color": "blue", "groups": ["Search engines"]},
       {"id": 9, "url": "clickbank.net", "color": "", "groups": null},
-      {"id": 10, "url": "cam.ac.uk", "color": "Green", "groups": null},
-      {"id": 11, "url": "hibu.com", "color": "Blue", "groups": ["Blogs"]},
-      {"id": 12, "url": "over-blog.com", "color": "Yellow", "groups": null},
-      {"id": 13, "url": "cafepress.com", "color": "Green", "groups": null}
+      {"id": 10, "url": "cam.ac.uk", "color": "green", "groups": null},
+      {"id": 11, "url": "hibu.com", "color": "blue", "groups": ["Blogs"]},
+      {"id": 12, "url": "over-blog.com", "color": "yellow", "groups": null},
+      {"id": 13, "url": "cafepress.com", "color": "green", "groups": null}
     ]
   }
 })
