67506cff2e05f79d14a7948ab32640e99ae07839
eoff93 <bane.dev101@gmail.com>
2016-07-17 20:37:44 +0200

    Use 'track by' were needed

--- app/app.js
+++ app/app.js
@@ -1,29 +1,8 @@
 angular.module('groupApp', ['checklist-model'])
 .factory('getData', function() {
   return {
-    groups: [
-      {"id": 1,"name": "Social sites", "color": "red", "sites": ["shop-pro.jp", "scribd.com"] },
-      {"id": 2,"name": "Search engines", "color": "", "sites": ["google.com", "linkedin.com"] },
-      {"id": 3,"name": "Blogs", "color": "green", "sites": ["live.com", "berkeley.edu", "hibu.com"] },
-      {"id": 4,"name": "Learning sites", "color": "orange", "sites": ["shop-pro.jp"] },
-      {"id": 5,"name": "Entertainment", "color": "red", "sites": [] }
-    ],
-
-    sites: [
-      {"id": 1, "url": "shop-pro.jp", "color": "red","groups": ["Social sites", "Learning sites"]},
-      {"id": 2, "url": "desdev.cn", "color": "blue", "groups": []},
-      {"id": 3, "url": "live.com", "color": "orange", "groups": ["Blogs"]},
-      {"id": 4, "url": "microsoft.com", "color": "red", "groups": []},
-      {"id": 5, "url": "scribd.com", "color": "yellow", "groups": ["Social sites"]},
-      {"id": 6, "url": "linkedin.com", "color": "", "groups": ["Search engines"]},
-      {"id": 7, "url": "berkeley.edu", "color": "green", "groups": ["Blogs"]},
-      {"id": 8, "url": "google.com", "color": "blue", "groups": ["Search engines"]},
-      {"id": 9, "url": "clickbank.net", "color": "", "groups": []},
-      {"id": 10, "url": "cam.ac.uk", "color": "green", "groups": []},
-      {"id": 11, "url": "hibu.com", "color": "blue", "groups": ["Blogs"]},
-      {"id": 12, "url": "over-blog.com", "color": "yellow", "groups": []},
-      {"id": 13, "url": "cafepress.com", "color": "green", "groups": []}
-    ]
+    groups: [],
+    sites: []
   }
 })
 .controller('MainController', function(getData) {
